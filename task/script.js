3bf8210b826885296914098813883d582dae276a
NurDavidli <shabnurbd@code.edu.az>
2024-03-28 19:27:10 +0400

    HOme

---
+++ task/script.js
@@ -0,0 +1,84 @@
+//TASK 1.
+// 1.Multiply 12 with 8, and console the result:
+
+
+let result = 12 * 8;
+console.log(result);
+
+// 2.Divide 10 by 2 add result to variables x, and console x:
+let Result = 10/2
+let x = Result+5
+console.log(Result);
+
+// 3.Create two variables called num1 and num2, assign the value 20 to num1 and value 17 to num2 then addition(+) them in console.
+
+let num1 = 20;
+let num2 = 17;
+console.log(num1+num2);
+
+/* 4.Create variables called name, surname and year assign values to them ('your name', 'your surname', ' your birth of year'),
+  then add them to varibale name (info) and console info. */
+
+let name = 'Nur';
+let surname = 'Davidli';
+let year = 2003;
+console.log(name + surname + year);
+
+// 5.Console the remainder(%) when 17 is divided by 12.
+// 6.Create a varibale called "city name" (camelCase). Assign value Baku, then call variable again
+//  and reassign value Gence,console result.
+
+//TASK 2.
+let a = 1, b = 1;
+let c = ++a;
+let d = b++;
+
+console.log(a);
+console.log(d);
+console.log(c);
+console.log(b);
+
+// TASK 3 /
+let a1 = 2;
+let x1 = 1 + (a1 *= 2);
+console.log(x1); 
+//Etməmiş özü cavab verdi heçnə anlamadım 
+
+//TASK 4     
+console.log('test1', test); 
+   {
+     let test = "something"
+     console.log('test2', test); 
+ }
+ console.log('test3', test);   
+
+
+console.log('test1', test); 
+ {
+     var test = "something"
+    console.log('test2', test); 
+}
+console.log('test3', test); 
+
+// TASK 5 
+
+//Find the type of all the following cases
+ let name1 = 'NUR'
+let surname1 = 'David'
+let year1 = 2003
+year = "2003"
+let city; 
+let qualification = null
+ let obj = { name: 'Nur' }
+let arr = ['n', 'u', 'r']
+
+//TASK 6 
+// 1.Create a object add your name, surname, age and city.
+// 2.Then change name to "Jhon"
+
+
+
+
+
+
+
